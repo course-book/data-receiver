f59d2c21d9082983882f1f1252d69f8e10129a2d
Zachary Taylor <ztayor@gmail.com>
2018-04-23 19:57:54 -0400

    changes to json attributes

--- src/handler/mongoHandler.js
+++ src/handler/mongoHandler.js
@@ -10,93 +10,74 @@ class MongoHandler {
   }
 
   receiveMessage(content) {
-    return new Promise((resolve, reject) => {
-      MongoClient.connect(this.host, (error, c) => {
-        if (error) {
-          this.logger.error("Could not connect to MongoClient", error.message);
-          request.post('http://433-12.csse.rose-hulman.edu:15672/#/', {
-            json: {
-              uuid: content.uuid,
-              statuscode: "100",
-              message: 'REGISTRATION_DOWN'
-            }
-          }, (error, response, body) => {
-            if (error) {
-              this.logger.error(error.message);
-            } else if (response.statusCode == 200) {
-              this.logger.info(body);
-            }
-          });
-          resolve(false);
-          return;
-        } else {
-          this.logger.info("connected to MongoClient");
-        }
+	  return new Promise((resolve, reject) => {
+		  MongoClient.connect(this.host, (error, c) => {
+			  if(error) {
+				  this.logger.error("Could not connect to MongoClient", error.message);
+				  request.post('http://433-12.csse.rose-hulman.edu:15672/#/',
+						  { json: {uuid:content.uuid, statuscode:"100", message:'REGISTRATION_DOWN'}},
+						  function (error, response, body) {
+							  if(!error && response.statusCode == 200) {
+								  this.logger.info(body)
+							  } else if (error) {
+								  this.logger.error(error.message)
+							  }
+						  })
+				  resolve(false);
+				  return;
+			  } else {
+				  this.logger.info("connected to MongoClient");
+			  }
 
-        const userdb = c.collection('users');
+			  const userdb = c.collection('users')
 
-        switch (content.action) {
-          case 'REGISTRATION':
-            const datum = {
-              username: content.username,
-              password: content.password,
-              uuid: content.uuid
-            };
-            if (userdb.find({"username": datum.username}).count() > 0) {
-              request.post('http://433-12.csse.rose-hulman.edu:15672/#/', {
-                json: {
-                  uuid: datum.uuid,
-                  statuscode: "400",
-                  message: 'REGISTRATION_FAILURE'
-                }
-              }, (error, response, body) => {
-                if (error) {
-                  this.logger.error(error.message)
-                } else if (response.statusCode == 200) {
-                  this.logger.info(body)
-                }
-                resolve(true);
-                return;
-              });
-            } else {
-              writeresult = userdb.insert(datum);
-              if (writeresult.nInserted == 1) {
-                request.post('http://433-12.csse.rose-hulman.edu:15672/#/', {
-                  json: {
-                    uuid: datum.uuid,
-                    statuscode: "200",
-                    message: "REGISTRATION_SUCCESS"
-                  }
-                }, (error, response, body) => {
-                  if (error) {
-                    this.logger.error(error.message)
-                  } else if (response.statusCode == 200) {
-                    this.logger.info(body)
-                  }
-                });
-              } else {
-                request.post('http://433-12.csse.rose-hulman.edu:15672/#/', {
-                  json: {
-                    uuid: datum.uuid,
-                    statuscode: "500",
-                    message: "REGISTRATION_FAILURE"
-                  }
-                }, (error, response, body) => {
-                  if (error) {
-                    this.logger.error(error.message)
-                  } else if (response.statusCode == 200) {
-                    this.logger.info(body)
-                  }
-                });
-              }
-              resolve(true);
-            }
-          default:
-            this.logger.warn(`Unexpected type ${content.action}.`);
-            return resolve(true);
-        }
-      });
-    });
+			  switch (content.action) {
+				  case 'REGISTRATION':
+					  const datum = {
+						  username:content.username;
+						  password:content.password;
+				  	  uuid:content.uuid;
+              action:"REGISTRATION";
+					  }
+					  if (userdb.find({"username":datum.username}).count() > 0) {
+						  request.post('http://433-12.csse.rose-hulman.edu:15672/#/',
+								  { json: {uuid:datum.uuid, statuscode:"400", message:'REGISTRATION_FAILURE', username:datum.username, action:datum.action}},
+								  function (error, response, body) {
+									  if(!error && response.statusCode == 200) {
+										  this.logger.info(body)
+									  } else if (error) {
+										  this.logger.error(error.message)
+									  }
+								  })
+						  resolve(true);
+					  } else {
+					  writeresult = userdb.insert(datum);
+					  if(writeresult.nInserted == 1) {
+						  request.post('http://433-12.csse.rose-hulman.edu:15672/#/',
+								  { json: {uuid:datum.uuid, statuscode:"200",message:"REGISTRATION_SUCCESS",username:datum.username, action:datum.action}},
+								  function (error, response, body) {
+									  if(!error && response.statusCode == 200) {
+										  this.logger.info(body)
+									  } else if (error) {
+										  this.logger.error(error.message)
+									  }
+								  }
+					  } else {
+						  request.post('http://433-12.csse.rose-hulman.edu:15672/#/',
+								  { json: {uuid:datum.uuid, statuscode:"500",message:"REGISTRATION_FAILURE",username:datum.username, action:datum.action}},
+								  function (error, response, body) {
+									  if(!error && response.statusCode == 200) {
+										  this.logger.info(body)
+									  } else if (error) {
+										  this.logger.error(error.message)
+									  }
+								  })
+					  }
+					  resolve(true);
+			  }
+			  }
+		  })
+	  })
   }
 }
 
