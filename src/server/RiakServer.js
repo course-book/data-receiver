3d7c86542a4a2dd0845837b34cf48821ff3fa425
David Lam <dav_lam@ymail.com>
2018-05-10 00:51:09 -0400

    registration stats for riak added

--- src/server/RiakServer.js
+++ src/server/RiakServer.js
@@ -1,9 +1,10 @@
+const Riak = require("basho-riak-client");
 const express = require("express");
 const bodyParser = require("body-parser");
 
 class RiakServer {
-  constructor(host, port, logger) {
-    this.host = host;
+  constructor(nodes, port, logger) {
+    this.nodes = nodes;
     this.port = port;
     this.logger = logger;
 
@@ -19,6 +20,35 @@ class RiakServer {
       const logTag = "PING";
       this.logger.info(`[ ${logTag} ] ping request`);
       response.status(200).send("pong");
+    });
+
+    app.get("/registration/:ip", (request, response) => {
+      const logTag = "REGISTRATION";
+      this.logger.info(`[ ${logTag} ] registration stats for ${request.params.ip}`);
+
+      const client = new Riak.Client(this.nodes, (error, c) => {
+        if (error) {
+          this.logger.error(`[ ${logTag} ] ${error.message}`);
+          response.status(500)
+            .send(error.message);
+          return;
+        }
+        this.logger.info(`[ ${logTag} ] successfully connected to riak`);
+
+        const options = {bucket: "REGISTRATION", bucketType: "counters", key: request.params.ip};
+        client.fetchCounter(options, (error, riakResponse, data) => {
+          if (error) {
+            this.logger.error(`[ ${logTag} ] ${error.message} with data ${data}`);
+            response.status(500)
+              .send(`Riak Error: ${error.message} with data ${data}`);
+              return;
+          }
+          this.logger.info(`[ ${logTag} ] response ${JSON.stringify(riakResponse)}`);
+          response.status(200)
+            .send(riakResponse);
+          return;
+        })
+      });
     });
 
     app.listen(this.port, (error) => {
