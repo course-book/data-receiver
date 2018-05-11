59cd579ca3c16f9a9fd343bb7d34586bf80a7922
Zachary Taylor <ztayor@gmail.com>
2018-05-10 22:12:57 -0400

    finished redis?

--- src/handler/RedisHandler.js
+++ src/handler/RedisHandler.js
@@ -8,17 +8,14 @@ class RedisHandler {
     this.host = host;
     this.logger = logger;
     this.responseEndpoint = responseEndpoint;
+    this.client = redis.createClient(host);
+    this.logger.info("[ REDIS ] connected to client")
 
     this.handleRedisDown = this.handleRedisDown.bind(this);
     this.respond = this.respond.bind(this);
   }
 
   recieveMessage(message) {
-    return new Promise((resolve, reject) => {
-      redis.createClient(this.host)
-        .then((client) => {
-          this.logger.info("[ REDIS ] connected to client");
-
           switch (content.action) {
             case "COURSE_UPDATE":
               dbset = "course";
@@ -32,58 +29,49 @@ class RedisHandler {
               this.logger.warn(`Unsupported action ${content.action}`);
               resolve(true);
           }
-        })
-    });
   }
-
-  handleCourseCreation(client, dbset, content, resolve) {
-    const logTag = "COURSE_CREATE"
-    this.logger.info(`[ ${logTag} ] handling course creation`);
-    key = "courses.name:"+content.name+":author:"+content.author;
-    exists = client.sismember(courses, key);
-    if(exists === 1) {
-        this.logger.info(`[ ${logTag} ] course ${content.name} by ${content.author} already exists`);
-        body.statusCode = 409;
-        body.message = "Course by that title already exists. Please try another.";
-    } else {
-        this.logger.info(`[ ${logTag} ] created course ${content.name} by ${content.author}`);
-        body.statusCode = 201;
-        body.message = "Course was successfully created.";
-        client.multi();
-        client.hmset(key, name, content.name, author, content.author, shortDescription, content.shortDescription,
-              description, content.description, sources, content.sources, reviews, [],
-              wish, content.wish);
-        client.sadd(dbset, key);
-        client.exec();
-        this.respond(logTag, body, resolve);
-    }
-  };
 
   handleCourseUpdate(client, content, resolve) {
     const logTag = "COURSE_INVALIDATE";
     this.logger.info(`[ ${logTag} ] invalidating my course`)
-    key = content.uuid;
-    client.srem("courses", key);
-    success = client.del(key);
-    if(success = 1) {
-        this.logger.info(`[ ${logTag} ] course invalidated with id ${content.uuid}`);
-    } else {
-        this.logger.info(`[ ${logTag} ] Course with id ${content.courseId} not found`);
-    }
+    key = content.courseId;
+    client.del(key, (err, res) => {
+      if(err) {
+        if(err.code === "NR_CLOSED") {
+          resolve(false);
+          return;
+        }
+        this.logger.error(`[ ${logTag} ] client passed invalid command ${err.command} with code ${err.code}`)
+      } else {
+        if(res = 1) {
+            this.logger.info(`[ ${logTag} ] course invalidated with id ${content.uuid}`);
+        } else {
+            this.logger.info(`[ ${logTag} ] Course with id ${content.courseId} not found`);
+        }
+      }
+    };
     resolve(true);
   }
 
   handleWishUpdate(client, content, resolve) {
     const logTag = "WISH_INVALIDATE";
-    this.logger.info(`[ ${logTag} ] invalidating my wish`)
-    key = content.uuid;
-    client.srem("wishes", key);
-    success = client.del(key);
-    if(success = 1) {
-        this.logger.info(`[ ${logTag} ] wish invalidated with id ${content.uuid}`);
-    } else {
-        this.logger.info(`[ ${logTag} ] wish with id ${content.courseId} not found`);
-    }
+    this.logger.info(`[ ${logTag} ] invalidating my course`)
+    key = content.wishId;
+    client.del(key, (err, res) => {
+      if(err) {
+        if(err.code === "NR_CLOSED") {
+          resolve(false);
+          return;
+        }
+        this.logger.error(`[ ${logTag} ] client passed invalid command ${err.command} with code ${err.code}`)
+      } else {
+        if(res = 1) {
+            this.logger.info(`[ ${logTag} ] wish invalidated with id ${content.uuid}`);
+        } else {
+            this.logger.info(`[ ${logTag} ] wish with id ${content.courseId} not found`);
+        }
+      }
+    };
     resolve(true);
   }
 
--- src/server/RedisServer.js
+++ src/server/RedisServer.js
@@ -1,10 +1,10 @@
 const express = require("express");
+const redis = require("redis")
 const bodyParser = require("body-parser");
 
 class RedisServer {
   constructor(host, port, logger) {
-    this.host = host;
-    this.port = port;
+    this.client = redis.createClient(port, host);
     this.logger = logger;
 
     this.listen = this.listen.bind(this);
@@ -16,6 +16,75 @@ class RedisServer {
     app.use(bodyParser.urlencoded({extended: false}));
 
     // TODO: specify endpoints
+
+    app.get("/course/fetch/:courseId", (request, response) => {
+      const logTag = "COURSE";
+      const courseId = request.params.id;
+
+      this.logger.info(`[ ${logTag} ] retrieving course with id ${courseId}`);
+      this.client.get(courseId, (err, res) => {
+        if(err || res === null){
+          handleMongoCourse(logTag, courseId, response)
+        } else {
+          response.status(200).send(JSON.parse(res));
+        }
+        });
+      });
+
+
+    app.get("/wish/fetch/:wishId", (request, response) => {
+      const logTag = "WISH";
+      const wishId = request.params.id;
+
+      this.logger.info(`[ ${logTag} ] retrieving wish with id ${wishId}`);
+      this.client.get(wishId, (err, res) => {
+        if(err || res === null){
+          handleMongoWish(logTag, wishId, response)
+        } else {
+          response.status(200).send(JSON.parse(res));
+        }
+        });
+      });
+
+    const handleMongoCourse = (logTag, courseId, response) => {
+      const options = {
+        method: "GET",
+        uri: encodeURI(`${MONGO_HOST}/course/${courseId}`),
+        json = true
+      }
+      rp(options)
+        .then((mongoResponse) => {
+          logger.info(`[ ${logTag} ] mongo responded with ${mongoResponse}`);
+          let message = mongoResponse;
+          client.set(courseId, mongoResponse.message)
+          response.status(mongoResponse.statusCode)
+            .send({message : mongoResponse.message});
+        })
+        .catch((error) => {
+          logger.error(`[ ${logTag} ] ${error.message}`);
+          response.status(500).send({message : error.message});
+        })
+    }
+
+    const handleMongoWish = (logTag, wishId, response) => {
+      const options = {
+        method: "GET",
+        uri: encodeURI(`${MONGO_HOST}/wish/${wishId}`),
+        json = true
+      }
+      rp(options)
+        .then((mongoResponse) => {
+          logger.info(`[ ${logTag} ] mongo responded with ${mongoResponse}`);
+          let message = mongoResponse;
+          client.set(courseId, mongoResponse.message)
+          response.status(mongoResponse.statusCode)
+            .send({message : mongoResponse.message});
+        })
+        .catch((error) => {
+          logger.error(`[ ${logTag} ] ${error.message}`);
+          response.status(500).send({message : error.message});
+        })
+    }
 
     app.listen(this.port, (error) => {
       if (error) {
